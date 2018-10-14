e13df0617e1b7049362b90410994b45e55061bfb
derrick b <dbozich1@gmail.com>
2018-10-14 19:35:51 +0000

    automatically backed up by learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -4,7 +4,7 @@ class SongsWorker
 
   def perform(songs_file)
     CSV.foreach(songs_file, headers: true) do |song|
-      Customer.create(email: song[0])
+      Customer.create(title: song[0])
     end
   end
 end
