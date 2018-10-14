850346437df5e061229c691b67ca4822b12e2c5e
derrick b <dbozich1@gmail.com>
2018-10-14 19:37:07 +0000

    automatically backed up by learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -4,7 +4,8 @@ class SongsWorker
 
   def perform(songs_file)
     CSV.foreach(songs_file, headers: true) do |song|
-      Customer.create(title: song[0])
+      s = Song.create(title: song[0])
+      s.artist = Artist.find_or_create_by(:name => song[1])
     end
   end
 end
